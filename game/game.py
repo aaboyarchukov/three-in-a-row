510aadfc49b6efc5d2c7dddc2b985c8c5daddfc0
aaboyarchukov <boyarchukov30@gmail.com>
2026-07-21 12:22:07 +0300

    three-in-a-row | уточнил АТД game

--- game/game.py
+++ game/game.py
@@ -1,11 +1,21 @@
-class Game():
+from any import Any
+
+class Game(Any):
+    # constructor
     def __init__(self):
-        self.board_matrix = None
+        pass    
     
     # commands
+    # pre-cond: game is not ending yet
+    # post-cond: game ending
     def end_game(self):
         pass
     
+    # pre-cond: 
+    #   - game is not ending yet
+    #   - dependecies are init
+    # post-cond: game ending
     def start_game(self):
         pass
+
     # requests
